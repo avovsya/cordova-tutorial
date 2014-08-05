d9b8d8c5ae232556e84eb3376e7f62d773715db0
Artem Vovsya <avovsya@gmail.com>
2014-08-05 18:40:02 +0300

    Finished main part of the tutorial

--- www/js/app.js
+++ www/js/app.js
@@ -4,9 +4,24 @@
   /* ---------------------------------- Local Variables ---------------------------------- */
   HomeView.prototype.template = Handlebars.compile($("#home-tpl").html());
   EmployeeListView.prototype.template = Handlebars.compile($("#employee-list-tpl").html());
+  EmployeeView.prototype.template = Handlebars.compile($("#employee-tpl").html());
+
+  var slider = new PageSlider($('body'));
+
   var service = new EmployeeService();
+
   service.initialize().done(function () {
-    $('body').html(new HomeView(service).render().$el);
+    router.addRoute('', function() {
+        slider.slidePage(new HomeView(service).render().$el);
+    });
+
+    router.addRoute('employees/:id', function(id) {
+        service.findById(parseInt(id)).done(function(employee) {
+            slider.slidePage(new EmployeeView(employee).render().$el);
+        });
+    });
+
+    router.start();
   });
 
   /* --------------------------------- Event Registration -------------------------------- */
